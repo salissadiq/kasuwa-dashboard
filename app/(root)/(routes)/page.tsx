8d244298a412a784763387157c7aa6007bb5f423
salissadiq <salissadiq@gmail.com>
2023-12-14 23:35:24 +0100

    Add a dashboard layout

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Modal from '@/components/ui/Modal'
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
+const SetupPage = () => {
+    const isOpen = useStoreModal((state) => state.ispoen)
+    const onOpen = useStoreModal((state) => state.onOpen)
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen()
+        }
+    }, [isOpen, onOpen])
+    return null
+}
+
+export default SetupPage
